65fe4402850766cb1548a4526e4323adf43f6b57
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-07-17 00:49:56 -0300

    Update PayloadParser.js
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

--- PayloadParser.js
+++ PayloadParser.js
@@ -1,152 +1,139 @@
-function parseUplink(device, payload) {
- 
-    var payloadb = payload.asBytes();
-    var decoded = Decoder(payloadb, payload.port)
-    env.log(decoded);
- 
-    // Store battery
-    if (decoded.battery != null) {
-         device.updateDeviceBattery({ percentage: decoded.battery });
- 
-    };
- 
-    // Store temperature
-    if (decoded.temperature != null) {
-        var sensor2 = device.endpoints.byAddress("2");
- 
-        if (sensor2 != null)
-            sensor2.updateTemperatureSensorStatus(decoded.temperature);
-    };
- 
+function parseUplink(device, payload){
+	var parsed = payload.asParsedObject();
+    env.log(parsed);
+
+    //var decoded = milesightDeviceDecode(data);
+    //env.log('Data Decodificada: ', decoded);
+
+    // Endpoint 1: Battery
+    var bat = device.endpoints.byAddress("1");
+    if (bat != null)
+        bat.updateGenericSensorStatus(parsed.battery);
+
     // Store humidity
-    if (decoded.humidity != null) {
-        var sensor3 = device.endpoints.byAddress("1");
- 
-        if (sensor3 != null)
-            sensor3.updateHumiditySensorStatus(decoded.humidity);
-    };
- 
+    var e = device.endpoints.byType(endpointType.humiditySensor);
+    if (e != null)
+        e.updateHumiditySensorStatus(parsed.humidity);
+
+    // Store temperature
+    e = device.endpoints.byType(endpointType.temperatureSensor);
+    if (e != null)
+        e.updateTemperatureSensorStatus(parsed.temperature);
+
 }
- 
-function buildDownlink(device, endpoint, command, payload) 
-{ 
-// This function allows you to convert a command from the platform 
-// into a payload to be sent to the device.
-// Learn more at https://wiki.cloud.studio/page/200
- 
-// The parameters in this function are:
-// - device: object representing the device to which the command will
-//   be sent. 
-// - endpoint: endpoint object representing the endpoint to which the 
-//   command will be sent. May be null if the command is to be sent to 
-//   the device, and not to an individual endpoint within the device.
-// - command: object containing the command that needs to be sent. More
-//   information at https://wiki.cloud.studio/page/1195.
- 
-// This example is written assuming a device that contains a single endpoint, 
-// of type appliance, that can be turned on, off, and toggled. 
-// It is assumed that a single byte must be sent in the payload, 
-// which indicates the type of operation.
- 
-/*
- payload.port = 25; 	 	 // This device receives commands on LoRaWAN port 25 
- payload.buildResult = downlinkBuildResult.ok; 
- 
- switch (command.type) { 
- 	 case commandType.onOff: 
- 	 	 switch (command.onOff.type) { 
- 	 	 	 case onOffCommandType.turnOn: 
- 	 	 	 	 payload.setAsBytes([30]); 	 	 // Command ID 30 is "turn on" 
- 	 	 	 	 break; 
- 	 	 	 case onOffCommandType.turnOff: 
- 	 	 	 	 payload.setAsBytes([31]); 	 	 // Command ID 31 is "turn off" 
- 	 	 	 	 break; 
- 	 	 	 case onOffCommandType.toggle: 
- 	 	 	 	 payload.setAsBytes([32]); 	 	 // Command ID 32 is "toggle" 
- 	 	 	 	 break; 
- 	 	 	 default: 
- 	 	 	 	 payload.buildResult = downlinkBuildResult.unsupported; 
- 	 	 	 	 break; 
- 	 	 } 
- 	 	 break; 
- 	 default: 
- 	 	 payload.buildResult = downlinkBuildResult.unsupported; 
- 	 	 break; 
- }
-*/
- 
-}
- 
-/**
-* Payload Decoder for The Things Network
-*
-* Copyright 2023 Milesight IoT
-*
-* @product EM320-TH
-*/
-function Decoder(bytes, port) {
-    return milesight(bytes);
-}
- 
-function milesight(bytes) {
+
+function milesightDeviceDecode(bytes) {
     var decoded = {};
- 
+
     for (var i = 0; i < bytes.length; ) {
         var channel_id = bytes[i++];
         var channel_type = bytes[i++];
- 
+
         // BATTERY
         if (channel_id === 0x01 && channel_type === 0x75) {
-            decoded.battery = bytes[i];
+            decoded.battery = readUInt8(bytes[i]);
             i += 1;
         }
         // TEMPERATURE
         else if (channel_id === 0x03 && channel_type === 0x67) {
-            // ℃
             decoded.temperature = readInt16LE(bytes.slice(i, i + 2)) / 10;
             i += 2;
- 
-            // ℉
-            // decoded.temperature = readInt16LE(bytes.slice(i, i + 2)) / 10 * 1.8 + 32;
-            // i +=2;
         }
         // HUMIDITY
         else if (channel_id === 0x04 && channel_type === 0x68) {
-            decoded.humidity = bytes[i] / 2;
+            decoded.humidity = readUInt8(bytes[i]) / 2;
             i += 1;
         }
-        // TEMPERATURE & HUMIDITY HISTROY
+        // HISTORICAL DATA
         else if (channel_id === 0x20 && channel_type === 0xce) {
-            var point = {};
-            point.timestamp = readUInt32LE(bytes.slice(i, i + 4));
-            point.temperature = readInt16LE(bytes.slice(i + 4, i + 6)) / 10;
-            point.humidity = bytes[i + 6] / 2;
+            var data = {};
+            data.timestamp = readUInt32LE(bytes.slice(i, i + 4));
+            data.temperature = readInt16LE(bytes.slice(i + 4, i + 6)) / 10;
+            data.humidity = readUInt8(bytes[i + 6]) / 2;
+
             decoded.history = decoded.history || [];
-            decoded.history.push(point);
+            decoded.history.push(data);
             i += 7;
         } else {
             break;
         }
     }
- 
+
     return decoded;
 }
- 
+
+
 /* ******************************************
-* bytes to number
-********************************************/
+ * bytes to number
+ ********************************************/
+
+ function readUInt8(bytes) {
+    return bytes & 0xff;
+}
+
+function readInt8(bytes) {
+    var ref = readUInt8(bytes);
+    return ref > 0x7f ? ref - 0x100 : ref;
+}
+
 function readUInt16LE(bytes) {
     var value = (bytes[1] << 8) + bytes[0];
     return value & 0xffff;
 }
- 
+
 function readInt16LE(bytes) {
     var ref = readUInt16LE(bytes);
     return ref > 0x7fff ? ref - 0x10000 : ref;
 }
- 
+
 function readUInt32LE(bytes) {
     var value = (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
     return (value & 0xffffffff) >>> 0;
 }
 
+function readProtocolVersion(bytes) {
+    var major = (bytes & 0xf0) >> 4;
+    var minor = bytes & 0x0f;
+    return "v" + major + "." + minor;
+}
+
+function readHardwareVersion(bytes) {
+    var major = bytes[0] & 0xff;
+    var minor = (bytes[1] & 0xff) >> 4;
+    return "v" + major + "." + minor;
+}
+
+function readFirmwareVersion(bytes) {
+    var major = bytes[0] & 0xff;
+    var minor = bytes[1] & 0xff;
+    return "v" + major + "." + minor;
+}
+
+function readTslVersion(bytes) {
+    var major = bytes[0] & 0xff;
+    var minor = bytes[1] & 0xff;
+    return "v" + major + "." + minor;
+}
+
+function readSerialNumber(bytes) {
+    var temp = [];
+    for (var idx = 0; idx < bytes.length; idx++) {
+        temp.push(("0" + (bytes[idx] & 0xff).toString(16)).slice(-2));
+    }
+    return temp.join("");
+}
+
+function readLoRaWANType(type) {
+    switch (type) {
+        case 0x00:
+            return "ClassA";
+        case 0x01:
+            return "ClassB";
+        case 0x02:
+            return "ClassC";
+        case 0x03:
+            return "ClassCtoB";
+        default:
+            return "Unknown";
+    }
+}
